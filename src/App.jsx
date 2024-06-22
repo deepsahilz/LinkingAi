615d9d141fce9ee2147b0f656223e00c9d0b3e9f
deepsahilz <deepsahil.online@gmail.com>
2024-06-22 18:39:31 +0530

    one

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='pt-36 flex justify-center flex-col text-4xl font-bold font-[neue]'>
       <img className='animate-spin-slow  mx-auto w-[8rem] mb-4' src={reactsvg}/>
-      <h1 className='mx-auto'>//write the code buddy</h1>
+      <h1 className='mx-auto'>//write the code buddy now</h1>
     </div>
   )
 }

f1d2db8307dc0c738bf0506ab52dd53b82a6b5fb
Sahildeep Singh <145499024+deepsahilz@users.noreply.github.com>
2025-01-06 23:07:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <div className='pt-36 flex justify-center flex-col text-4xl font-bold font-[neue]'>
       <img className='animate-spin-slow  mx-auto w-[8rem] mb-4' src={reactsvg}/>
       <h1 className='mx-auto'>//write the code buddy now 1</h1>
+      //just a commit
     </div>
   )
 }
